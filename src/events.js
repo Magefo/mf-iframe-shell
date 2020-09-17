d3bc853eca5965f66d6fe50d5eb08fa335bfa211
Miguel <miguel.ingsistemas@gmail.com>
2020-09-17 09:39:22 -0500

    Configure ports

--- src/events.js
+++ src/events.js
@@ -1,21 +1,22 @@
 const mfAngular = document.getElementById("mf-angular");
 const mfReact = document.getElementById("mf-react");
 
+const angularOrigin = "http://localhost:4002";
+const reactOrigin = "http://localhost:4003";
+
 export function initEvents() {
-  window.addEventListener("message", ({origin, data}) => {
-    if (
-      !["http://localhost:4200", "http://localhost:3002"].includes(origin)
-    ) {
+  window.addEventListener("message", ({ origin, data }) => {
+    if (![angularOrigin, reactOrigin].includes(origin)) {
       return;
     }
 
     mfAngular.contentWindow.postMessage(
       { app: "MF_SHELL", event: data.event },
-      "http://localhost:4200"
+      angularOrigin
     );
     mfReact.contentWindow.postMessage(
       { app: "MF_SHELL", event: data.event },
-      "http://localhost:3002"
+      reactOrigin
     );
   });
 }
